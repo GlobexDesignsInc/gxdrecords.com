9e8f3a6c7127aa09101114c8edb38f15a32c2cf2
Ev Haus <globexdesigns@gmail.com>
2018-09-02 20:03:26 -0700

    New server

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ const Deploy = require('@globexdesigns/deploy');
 const D = new Deploy({
 	directories: ['.next', 'static'],
 	files: ['package.json', 'server.js', 'yarn.lock'],
-	host: '159.89.129.218',
+	host: '159.65.99.81',
 	private_key_path: '/home/globex/.ssh/id_rsa',
 	target_dir: '/var/www',
 	user: 'root',
