b80a45cb31ca09546808381022d7c25968f3430c
Ev Haus <ev@zenhub.com>
2019-09-07 14:16:01 -0700

    Add GXD0004 and GXD0004

--- releases.js
+++ releases.js
@@ -1,6 +1,6 @@
 // @flow strict
 
-export type StoreType = 'Beatport' | 'iTunes' | 'Amazon';
+export type StoreType = 'Beatport' | 'iTunes' | 'Amazon' | 'Spotify';
 
 export type ReleaseType = {|
 	artist: string,
@@ -35,14 +35,14 @@ const releases: Array<ReleaseType> = [{
 		'By The Way': '04:28',
 	},
 	buy: {
-		Beatport: {
-			url: 'http://www.beatport.com/release/moving-forward/883056',
+		Spotify: {
+			url: 'https://open.spotify.com/album/6ZPFSOyvbCs12SyqEQ8aIv',
 		},
 		iTunes: {
 			url: 'https://itunes.apple.com/ca/album/moving-forward/1227264741',
 		},
-		Amazon: {
-			url: 'https://www.amazon.com/Moving-Forward-Evgueni-Naverniouk/dp/B06ZY82ZQN',
+		Beatport: {
+			url: 'http://www.beatport.com/release/moving-forward/883056',
 		},
 	},
 }, {
@@ -57,14 +57,61 @@ const releases: Array<ReleaseType> = [{
 		'Fragile Existence [Game Edit]': '00:48',
 	},
 	buy: {
-		Beatport: {
-			url: 'http://www.beatport.com/release/pandemic-2-5-soundtrack/911464',
+		Spotify: {
+			url: 'https://open.spotify.com/album/37dugCepVfj2Hg4SzNd3us',
 		},
 		iTunes: {
 			url: 'http://itunes.apple.com/ca/album/pandemic-2.5-soundtrack-ep/id525558248',
 		},
+		Beatport: {
+			url: 'http://www.beatport.com/release/pandemic-2-5-soundtrack/911464',
+		},
+	},
+}, {
+	artist: 'Evgueni Naverniouk',
+	catalog: 'GXD0003',
+	title: 'Scapefall (Original Game Soundtrack)',
+	type: 'ep',
+	date: '2019-08-25',
+	tracks: {
+		Scapefall: '01:28',
+		'Through the Alleys': '02:31',
+		'The Scouring': '04:00',
+		'Chasing the Fox': '03:55',
+	},
+	buy: {
+		Spotify: {
+			url: 'https://open.spotify.com/album/046kM6IAR79ESQBADVLsfn',
+		},
+		iTunes: {
+			url: 'https://music.apple.com/ca/album/scapefall-original-game-soundtrack-ep/1478500932',
+		},
 		Amazon: {
-			url: 'https://www.amazon.com/Pandemic-2-5-Soundtrack-Evgueni-Naverniouk/dp/B071R493S3/',
+			url: 'https://www.amazon.com/Scapefall-Soundtrack-Evgueni-Naverniouk/dp/B07X5828GB/',
+		},
+	},
+}, {
+	artist: 'Evgueni Naverniouk',
+	catalog: 'GXD0004',
+	title: 'Kingdom Keeper (Original Game Soundtrack)',
+	type: 'ep',
+	date: '2019-08-25',
+	tracks: {
+		Entrance: '00:15',
+		Swell: '00:08',
+		'Main Menu': '00:36',
+		'Adventure Theme': '01:22',
+		'Boss Theme': '00:20',
+	},
+	buy: {
+		Spotify: {
+			url: 'https://open.spotify.com/album/4Z2BfCvRrL4pYUIYemZGkv',
+		},
+		iTunes: {
+			url: 'https://music.apple.com/ca/album/kingdom-keeper-original-game-soundtrack-ep/1478500392',
+		},
+		Amazon: {
+			url: 'https://www.amazon.com/Kingdom-Keeper-Original-Game-Soundtrack/dp/B07X6JKQ43',
 		},
 	},
 }].reverse();
